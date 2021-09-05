6f5e5c9317093900d43cc4d1933fdd6e2106c052
Manuel Alvarez <mjalvarezx@alumno.uspg.edu.gt>
2021-09-05 16:47:00 -0600

    ejemplos databinding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProyectoConAngular';
+  title2 = 'Prueba con String Interpolation';
+  title3 = 'funcion en String Interpolation';
+  getTitulo(){
+    return this.title3;
+  }
+
+  urlimg='https://th.bing.com/th/id/OIP.wUV-QqfzXA1NMKoVdneLVgAAAA?pid=ImgDet&rs=1';
+  
+  texto='prueba de property binding';
+  
+  color='blue';
+
+  numero=0;
+  numero1=0;
+  numero2=0;
+  numero3=0;
+  
+  incrementar(){
+    this.numero++;
+  }
+
+  decrementar(){
+    this.numero--;
+  }
+
+  suma():number{
+    return this.numero2 + this.numero3;
+  }
+
 }
